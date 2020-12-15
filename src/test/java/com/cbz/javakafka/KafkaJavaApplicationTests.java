3c7d34bbf1990e824eb35482acb4cc8afdcabf32
codeboz <codeboz@outlook.com>
2020-12-15 10:42:08 +0300

    KafkaJava Application initialized

---
+++ src/test/java/com/cbz/javakafka/KafkaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbz.javakafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
